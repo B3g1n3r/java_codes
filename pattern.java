7b23a14cfd73f5a333e957356fe768f92ff1c378
B3g1n3r <eswaransiva487@gmail.com>
2022-09-02 23:39:03 +0530

    pattern_program

---
+++ pattern.java
@@ -0,0 +1,12 @@
+public class pattern{
+public static void main(String args[]){
+for(int q=1;q<=5;q++){
+for(int w=1;w<=q;w++){
+System.out.print("*");
+
+}
+System.out.println(" ");
+}//1st 
+
+}
+}
